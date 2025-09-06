f02e8d04bf17c1c2701d095cb5c9b5299ac94e05
Ahmad Maulana Ardiansyah <na09lana@gmail.com>
2025-09-06 12:07:57 +0700

    Update suhuu.py

--- suhuu.py
+++ suhuu.py
@@ -1,34 +1,38 @@
-#Rumus Konversi Suhu
 def konversi_suhu(nilai, dari, ke):
-  nilai = float(nilai)
-  dari = dari.upper()
-  ke = ke.upper()
-if dari == 'C':
-    if ke == 'F':
-        hasil = (suhu * 9/5) + 32
-    elif ke == 'K':
-        hasil = suhu + 273.15
-    elif ke == 'C':
-        hasil = suhu
+    nilai = float(nilai)
+    dari = dari.upper()
+    ke = ke.upper()
+
+    if dari == 'C':
+        if ke == 'F':
+            hasil = (nilai * 9/5) + 32
+        elif ke == 'K':
+            hasil = nilai + 273.15
+        elif ke == 'C':
+            hasil = nilai
+        else:
+            hasil = "Invalid target unit"
+
+    elif dari == 'F':
+        if ke == 'C':
+            hasil = (nilai - 32) * 5/9
+        elif ke == 'K':
+            hasil = (nilai - 32) * 5/9 + 273.15
+        elif ke == 'F':
+            hasil = nilai
+        else:
+            hasil = "Invalid target unit"
+
+    elif dari == 'K':
+        if ke == 'C':
+            hasil = nilai - 273.15
+        elif ke == 'F':
+            hasil = (nilai - 273.15) * 9/5 + 32
+        elif ke == 'K':
+            hasil = nilai
+        else:
+            hasil = "Invalid target unit"
     else:
-        hasil = "Invalid target unit"
-elif dari == 'F':
-    if ke == 'C':
-        hasil = (suhu - 32) * 5/9
-    elif ke == 'K':
-        hasil = (suhu - 32) * 5/9 + 273.15
-    elif ke == 'F':
-        hasil = suhu
-    else:
-        hasil = "Invalid target unit"
-elif dari == 'K':
-    if ke == 'C':
-        hasil = suhu - 273.15
-    elif ke == 'F':
-        hasil = (suhu - 273.15) * 9/5 + 32
-    elif ke == 'K':
-        hasil = suhu
-    else:
-        hasil = "Invalid target unit"
-else:
-    hasil = "Invalid source unit"
+        hasil = "Invalid source unit"
+
+    return hasil
