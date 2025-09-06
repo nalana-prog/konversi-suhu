eda74f31d243cf2833de98537f24754c60330238
Ahmad Maulana Ardiansyah <na09lana@gmail.com>
2025-09-06 12:21:15 +0700

    Update suhuu.py

--- suhuu.py
+++ suhuu.py
@@ -1,29 +1,30 @@
 def konversi_suhu(suhu, dari, ke):
-  suhu = float(suhu)
-  dari = dari.lower()
-  ke = ke.lower()
+    suhu = float(suhu)
+    dari = dari.lower()
+    ke = ke.lower()
 
-  if dari == 'c':
-    if ke == 'f':
-      return suhu * 9/5 + 32
-    elif ke == 'k':
-      return suhu + 273.15
-    else:
-      return suhu
+    if dari == 'c':
+        if ke == 'f':
+            return suhu * 9/5 + 32
+        elif ke == 'k':
+            return suhu + 273.15
+        elif ke == 'c':
+            return suhu
 
-  if dari == 'f':
-    if ke == 'c':
-      return (suhu - 32) * 5/9
-    elif ke == 'k':
-      return (suhu - 32) * 5/9 + 273.15
-    else:
-      return suhu
+    elif dari == 'f':
+        if ke == 'c':
+            return (suhu - 32) * 5/9
+        elif ke == 'k':
+            return (suhu - 32) * 5/9 + 273.15
+        elif ke == 'f':
+            return suhu
 
-  if dari == 'k':
-    if ke == 'c':
-      return suhu - 273.15
-    elif ke == 'f':
-      return (suhu - 273.15) * 9/5 + 32
-    else:
-      return suhu
+    elif dari == 'k':
+        if ke == 'c':
+            return suhu - 273.15
+        elif ke == 'f':
+            return (suhu - 273.15) * 9/5 + 32
+        elif ke == 'k':
+            return suhu
 
+    return "Satuan tidak valid!"
